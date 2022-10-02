8e587f5642b65c73a4fd711c32924bf90e91a9ed
Иван Асманов <Asmanovvv@yandex.ru>
2022-10-03 02:29:09 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+import java.sql.SQLOutput;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error errordefault = new Error();
+        Error errorargfals = new Error(false, 0, "Ложь");
+        Error errorargtrue = new Error(true, 1, "Истина");
+        errordefault.printInfo();
+        errorargfals.printInfo();
+        errorargtrue.printInfo();
+    }
+}

cb011971854e63e6a1d33480cb45ac0ffc95a3a8
Иван Асманов <Asmanovvv@yandex.ru>
2022-10-21 14:07:50 +0300

    6. Изменить программу Tracker из 2-го модуля [#10039 #353785]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -30,13 +30,12 @@ public class Tracker {
     }
 
     public List<Item> findAll() {
-        return items;
+        return List.copyOf(items);
     }
 
     public List<Item> findByName(String key) {
         List<Item> rsl = new ArrayList<>();
-        for (int index = 0; index < items.size(); index++) {
-            Item item = items.get(index);
+        for (Item item : items) {
             if (item.getName().equals(key)) {
                 rsl.add(item);
             }

fb123b82a42f08ab9678261c8274c1a498494bc0
Иван Асманов <Asmanovvv@yandex.ru>
2022-10-30 03:13:01 +0300

    3. Компаратор для строк [#35008 #355799]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -7,17 +7,6 @@ import static org.assertj.core.api.Assertions.offset;
 public class TriangleTest {
 
     @Test
-    public void when00and40and04Then8() {
-        Point a = new Point(0, 0);
-        Point b = new Point(4, 0);
-        Point c = new Point(0, 4);
-        Triangle triangle = new Triangle(a, b, c);
-        double rsl = triangle.area();
-        double expected = 8;
-        assertThat(rsl).isCloseTo(expected, offset(0.001));
-    }
-
-    @Test
     public void when00and04and05Then1() {
         Point a = new Point(0, 0);
         Point b = new Point(0, 4);

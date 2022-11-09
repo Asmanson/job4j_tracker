04a63ebe37c544a7bf9c07d29488f9478ce4ae22
Иван Асманов <Asmanovvv@yandex.ru>
2022-11-10 00:28:38 +0300

    Просто так

---
+++ src/main/java/ru/job4j/map/AnalyzeByMap.java
@@ -0,0 +1,10 @@
+package ru.job4j.map;
+
+import java.util.*;
+import java.util.LinkedHashMap;
+
+public class AnalyzeByMap {
+    public static String averageScore(String art) {
+        return art;
+    }
+}
---
+++ src/test/java/ru/job4j/map/AnalyzeByMapTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.map;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+
+class AnalyzeByMapTest {
+    @Test
+    public void whenAverageScore() {
+        String arc = "gh";
+        String ex = "gh";
+        assertThat(arc).isEqualTo(ex);
+    }
+}
